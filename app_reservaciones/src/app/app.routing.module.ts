e1b75aba658ea1e9be72086f63ca88fe6b80fd81
MenWic <lamr467@gmail.com>
2024-09-23 18:21:35 -0600

    Creacion de App con Angular

---
+++ app_reservaciones/src/app/app.routing.module.ts
@@ -0,0 +1,13 @@
+
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  // Aquí defines tus rutas
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }

0a826c911b6ca55210ac2746e658fc328ba92fb0
FernandoJRR <fercitotron@gmail.com>
2024-09-24 19:24:48 -0600

    feat: add base api spring boot

---
+++ api_reservaciones/src/main/java/com/university/ApiReservacionesProyecto2AyDApplication.java
@@ -0,0 +1,13 @@
+package com.university;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiReservacionesProyecto2AyDApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiReservacionesProyecto2AyDApplication.class, args);
+	}
+
+}

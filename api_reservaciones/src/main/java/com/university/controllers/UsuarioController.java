efe4ddedeaa95c530507409e126d5d254a3881fb
FernandoJRR <fercitotron@gmail.com>
2024-10-31 13:10:46 -0600

    fix

--- api_reservaciones/src/main/java/com/university/controllers/UsuarioController.java
+++ api_reservaciones/src/main/java/com/university/controllers/UsuarioController.java
@@ -215,7 +215,7 @@ public class UsuarioController {
                     @Content(mediaType = "application/json", schema = @Schema(implementation = Usuario.class)) }),
             @ApiResponse(responseCode = "400", description = "Solicitud incorrecta")
     })
-    @PostMapping("/usuario/public/crearAyudante")
+    @PostMapping("/usuario/private/crearAyudante")
     public ResponseEntity<?> crearAyudante(@RequestBody CreateUsuarioDto crear) {
         try {
             Usuario respuesta = usuarioService.crearAyudante(crear);
